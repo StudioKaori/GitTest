ee26e79c5706bd69a8a85c964f2042fea1635a41
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-10-14 15:22:29 +0200

    Initial Commit

---
+++ gitTest/gitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestApp.swift
+//  gitTest
+//
+//  Created by Kaori Persson on 2022-10-14.
+//
+
+import SwiftUI
+
+@main
+struct gitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
