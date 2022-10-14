2ad0c1ee97fa5dabdc8a608e0df3f66e50cfba59
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-10-14 15:32:40 +0200

    Add: color pink

--- gitTest/ContentView.swift
+++ gitTest/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
 					Color.blue
-					
+					Color.pink
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
